d4fa912bf63cb25724d4ce2550cf3bd1d05a2c85
anthonydibi <ajdibiaggio@gmail.com>
2023-02-21 14:08:27 -0600

    Replaced keyboard responses w/ button responses

--- src/Experiment.js
+++ src/Experiment.js
@@ -1,6 +1,5 @@
 import { initJsPsych } from "jspsych";
 import jsPsychHtmlButtonResponse from '@jspsych/plugin-html-button-response';
-import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
 
 export default function Experiment() {
     const jsPsych = initJsPsych({ on_finish: function() {
@@ -19,21 +18,23 @@ export default function Experiment() {
     //type is the interaction plugin to use - we are using the html keyboard response here which means the stimulus is HTML
     //and the response is a keyboard input. We don't provide input choices here since the user can press any key to begin
     const welcome = {
-        type: jsPsychHtmlKeyboardResponse,
-        stimulus: "Welcome to the experiment. Press any key to begin."
+        type: jsPsychHtmlButtonResponse,
+        stimulus: 'Welcome to the experiment. Press "continue" to advance.',
+        choices: ['Continue'],
     }
     timeline.push(welcome);
 
     const instructions = {
-        type: jsPsychHtmlKeyboardResponse,
-        stimulus: 'You have three choices for rating each face: positive, indicating that the person seems happy, pleased, or delighted; neutral, indicating a lack of positive or negative emotional expression; or negative, indicating that the person seems sad, afraid, angry, or disgusted. You will see faces only for a brief moment, so please respond as fast as possible. Select the most accurate description for the emotional expression on each face. If the face contains a positive emotional expression, press the "positive" button. If the face contains a neutral emotional expression, press the "neutral" button. If the face contains a negative emotional expression, press the "negative" button. Press any key to continue.',
+        type: jsPsychHtmlButtonResponse,
+        stimulus: 'You have three choices for rating each face: positive, indicating that the person seems happy, pleased, or delighted; neutral, indicating a lack of positive or negative emotional expression; or negative, indicating that the person seems sad, afraid, angry, or disgusted. You will see faces only for a brief moment, so please respond as fast as possible. Select the most accurate description for the emotional expression on each face. If the face contains a positive emotional expression, press the "positive" button. If the face contains a neutral emotional expression, press the "neutral" button. If the face contains a negative emotional expression, press the "negative" button. Press "continue" to advance.',
+        choices: ['Continue'],
     }
     timeline.push(instructions);
 
     const trial = {
         type: jsPsychHtmlButtonResponse,
         stimulus: jsPsych.timelineVariable("stimulus"),
-        choices: ['positive', 'neutral', 'negative']
+        choices: ['Positive', 'Neutral', 'Negative']
     }
 
     //a procedure uses a timeline variable to show a repeated series of trials. It runs the timeline once for each element in the timeline
